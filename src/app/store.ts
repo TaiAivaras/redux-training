435657c608568a6870b086d3ca443e7565318c6c
Aivaras Gluodas <aivaras.gluodas@devbridge.com>
2021-04-27 16:19:55 +0300

    After training

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,18 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga'
 import counterReducer from '../features/counter/counterSlice';
+import {counterSagas} from "../features/counter/counterSagas";
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        counter: counterReducer,
+    },
+    middleware: [sagaMiddleware]
 });
+
+sagaMiddleware.run(counterSagas)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
---
+++ src/features/counter/counterSagas.ts
@@ -0,0 +1,38 @@
+import {call, delay, put, race, takeEvery} from "@redux-saga/core/effects";
+import {incrementAsyncFail, incrementAsyncStart, incrementAsyncSuccess} from "./counterSlice";
+import {fetchCount} from "./counterAPI";
+
+function* apiIncrement() {
+    try {
+        const {result} = yield race({
+            result: call(fetchCount, 2, 4000),
+            timeout: delay(3000),
+        })
+
+        console.log(result)
+
+
+        if (result) {
+            yield put({
+                type: incrementAsyncSuccess.type,
+                payload: result.data
+            })
+        } else {
+            yield put({
+                type: incrementAsyncFail.type,
+            })
+        }
+
+
+    } catch (e) {
+        yield put({
+            type: incrementAsyncFail.type,
+        })
+    }
+
+
+}
+
+export function* counterSagas() {
+    yield takeEvery(incrementAsyncStart.type, apiIncrement)
+}
